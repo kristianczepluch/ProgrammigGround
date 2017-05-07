f95e4e74fc99e440bb33c5583b6cde677d123b8b
kristian czepluch <k.czepluch@yahoo.de>
2017-05-07 03:18:44 +0200

    Kapitel 5 Übungen

---
+++ C++ Tasks/Kapitel 5/Übungen/Übung 2.cpp
@@ -0,0 +1,30 @@
+//Bjarne Stroustrup Einführung in die Programmierung mit C++
+// Kapitel 5, S.191 Übungen 2.)
+// Korrigiertes Programm welches Celsius in Kelvin umrechnet und ungültige Eingaben des Benutzers abfängt
+
+#include "std_lib_facilities.h"
+#include <limits>
+
+double ctok(double c) {
+	double k = c + 273.15;
+	return k;
+}
+
+int main() {
+	double k; 
+	double c;
+	
+	std::cout << "Bitte geben Sie die gewünschte Tempreatur in Celsius ein: ";
+	std::cin >> c;
+	while (std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore(1000, '\n');
+		std::cout << "Ungültige Eingabe! Bitte geben sie einen Double Wert ein: ";
+		std::cin >> c;
+	}
+	
+	k = ctok(c);
+	std::cout << "Die umgerechnete Eingabe in Kelvin lautet: " << k << endl;
+	keep_window_open();
+	return 0;
+}
---
+++ C++ Tasks/Kapitel 5/Übungen/Übung3.cpp
@@ -0,0 +1,38 @@
+//Bjarne Stroustrup Einführung in die Programmierung mit C++
+// Kapitel 5, S.191 Übungen 3.)
+// Program aus der letzten Aufgabe dahingehend korrigiert, dass ein Fehler gemeldet wird, wenn die Eingabe den absoluten Nullpunkt übersteigt. Änderung sollte in der Main durchgeführt werden.
+
+#include "std_lib_facilities.h"
+#include <limits>
+
+double ctok(double c) {
+	double k = c + 273.15;
+	return k;
+}
+
+int main() {
+	try {
+		double k;
+		double c;
+
+		std::cout << "Bitte geben Sie die gewünschte Tempreatur in Celsius ein: ";
+		std::cin >> c;
+		while (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "Ungültige Eingabe! Bitte geben sie einen Double Wert ein: ";
+			std::cin >> c;
+		}
+
+		if (c < -273.15) error("Ungültige Eingabe, da die Temperatur die niedrigste Temperatur übersteigt!");
+		k = ctok(c);
+		std::cout << "Die umgerechnete Eingabe in Kelvin lautet: " << k << endl;
+		keep_window_open();
+		return 0;
+	}
+	catch (runtime_error& s) {
+		std::cerr << s.what() << std::endl;
+		keep_window_open();
+		return 1;
+	}
+}
