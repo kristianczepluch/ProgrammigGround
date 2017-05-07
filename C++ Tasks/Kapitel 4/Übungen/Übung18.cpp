8c6d39403e38ad688729157a02341057b1919241
Kristian Czepluch <k.czepluch@gmx.net>
2017-05-07 17:07:51 +0200

    Update Übung18.cpp

--- C++ Tasks/Kapitel 4/Übungen/Übung18.cpp
+++ C++ Tasks/Kapitel 4/Übungen/Übung18.cpp
@@ -1,6 +1,6 @@
-//Bjarne Stroustrup Einführung in die Programmierung mit C++
+//Bjarne Stroustrup EinfÃ¼hrung in die Programmierung mit C++
 // Kapitel 4, Aufgabe 18 S.155
-// Program, welches einem quadratische Gleichungen löst!
+// Program, welches einem quadratische Gleichungen lÃ¶st!
 
 #include "../facilities.h"
 
@@ -13,20 +13,20 @@ int main() {
 	double x1;
 	double x2;
 
-	std::cout << "Bitte geben Sie die Werte a,b,c getrennt mit einer Leertaste für die Gleischung ax²+bx+c=0 ein:  " << std::endl;
+	std::cout << "Bitte geben Sie die Werte a,b,c getrennt mit einer Leertaste fÃ¼r die Gleichung axÂ²+bx+c=0 ein:  " << std::endl;
 	std::cin >> a >> b >> c;
 
 	if ((2 * a) == 0) {
-		std::cout << "Die Gleichung hat keine Lösungen!" << std::endl;
+		std::cout << "Die Gleichung hat keine LÃ¶sungen!" << std::endl;
 	}
 	else if (((b*b) - (4 * a*c))<0) {
-		std::cout << "Die Gleichung hat keine Lösungen!" << std::endl;
+		std::cout << "Die Gleichung hat keine LÃ¶sungen!" << std::endl;
 	}
 	else {
 		x1 = (-b + sqrt((b*b) - (4 * a*c))) / (2 * a);
 		x2 = (-b - sqrt((b*b) - (4 * a*c))) / (2 * a);
 
-		std::cout << "Die Lösungen lauten: x1= " << x1 << ", x2= " << x2 << std::endl;
+		std::cout << "Die LÃ¶sungen lauten: x1= " << x1 << ", x2= " << x2 << std::endl;
 	}
 	keep_window_open();
 	return 0;
