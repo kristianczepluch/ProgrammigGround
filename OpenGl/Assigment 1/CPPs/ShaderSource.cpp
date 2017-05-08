1c0cad6448790589cc4545b90a865dc41288bb9e
kristian czepluch <k.czepluch@yahoo.de>
2017-05-08 02:07:30 +0200

    First OpenGl C++ App

---
+++ OpenGl/Assigment 1/CPPs/ShaderSource.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <glew.h>
+
+GLchar* getTriangleVertexSource() {
+	GLchar* vertexShaderSource =
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 position;\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+		"}";
+
+	return vertexShaderSource;
+}
+
+GLchar* getTriangleFragmentSource() {
+	GLchar* fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"color = vec4(0.0f,0.0f,0.0f, 1.0f);\n"
+		"}";
+	return fragmentShaderSource;
+        
+}
---
+++ OpenGl/Assigment 1/CPPs/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#define GLEW_STATIC
+#include <glew.h>
+#include <glfw3.h>
+#include "Window.h";
+#include "ShaderUtils.h";
+#include "ShaderSource.h";
+#include "Triangle.h";
+
+
+int main()
+{
+	 //Create Window
+	 GLFWwindow* window = createWindow(); 
+
+	 // Get the ShaderSource
+	 GLchar* vertexShaderSource = getTriangleVertexSource();
+	 GLchar* fragmentShaderSource = getTriangleFragmentSource();
+
+	 //Compile Shader
+	 GLuint vertexShader = createVertexShader(vertexShaderSource);
+	 GLuint fragmentShader = createFragmentShader(fragmentShaderSource);
+
+	 //Link Shader
+	 GLuint shaderProgram = createShaderProgram(vertexShader, fragmentShader);
+
+	 // Clean Shader
+	 glDeleteShader(vertexShader);
+	 glDeleteShader(fragmentShader);
+
+	// Creating a FloatArray with our VertexData
+	 GLfloat vertices[] = {
+		 // Positions         
+		 0.0f, 0.5f, 0.0f,                
+		-0.5f, 0.0f, 0.0f,               
+		 0.5f, 0.0f, 0.0f,               
+		-0.5f,-1.0f, 0.0f,                
+		 0.5f,-1.0f, 0.0f                
+	 };
+
+	 GLuint indices[] = {
+		 3,1,2,
+		 0,1,2,
+		 3,4,2,
+		 1,3,4
+
+	 };
+
+
+	 GLuint VBO, VAO, EBO;
+	 glGenVertexArrays(1, &VAO);
+	 glGenBuffers(1, &VBO);
+	 glGenBuffers(1, &EBO);
+	 // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	 glBindVertexArray(VAO);
+	 glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	 glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	 glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	
+	 glEnableVertexAttribArray(0);
+
+	 glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+	 glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
+
+
+						   // Uncommenting this call will result in wireframe polygons.
+						   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+	 glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	 glClear(GL_COLOR_BUFFER_BIT);
+	 glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
+}
+
+
