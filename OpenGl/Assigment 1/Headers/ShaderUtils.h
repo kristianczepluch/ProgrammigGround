84de3a054217fdabafd7cf9f249b87563ee5ccca
unknown <k.czepluch@yahoo.de>
2017-06-17 18:41:22 +0200

    Assignments added

--- OpenGl/Assigment 1/Headers/ShaderUtils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <glew.h>
-
-GLuint createVertexShader(GLchar* source);
-GLuint createFragmentShader(GLchar* source);
-GLuint createShaderProgram(GLuint vertexShader, GLuint fragmentShader);
